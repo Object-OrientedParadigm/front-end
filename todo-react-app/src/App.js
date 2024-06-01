46310482bfe66b733b87f944ae8efedc12e241e5
kahyun0255 <kkh9946@naver.com>
2024-06-02 00:08:21 +0900

    완료한 목록만 보기 기능 오류 수정

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
             items: [],
             loading: true,
             checkedCount: 0,
-            showCompleted: false,
+            showCompleted: true,
         };
     }
 
@@ -55,7 +55,7 @@ class App extends React.Component {
 
     render() {
         const { items, checkedCount, showCompleted } = this.state;
-        const filteredItems = showCompleted ? items.filter(item => item.done) : items;
+        const filteredItems = !showCompleted ? items.filter(item => item.done) : items;
 
         var todoItems = filteredItems.length > 0 && (
             <Paper style={{ margin: 16 }}>
